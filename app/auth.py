eb779169ddfa38c18703af3825745c11db1851e6
szid2006 <sz.szabosyd@gmail.com>
2026-01-06 16:16:44 +0100

    Create auth.py

---
+++ app/auth.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, session, redirect, render_template
+
+auth = Blueprint("auth", __name__)
+
+USERNAME = "admin"
+PASSWORD = "1234"
+
+@auth.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if (
+            request.form.get("username") == USERNAME
+            and request.form.get("password") == PASSWORD
+        ):
+            session["logged_in"] = True
+            return redirect("/shifts")
+    return render_template("login.html")
+
+
+def login_required(fn):
+    def wrapper(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect("/")
+        return fn(*args, **kwargs)
+    return wrapper
