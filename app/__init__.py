1c58487b1877f7372321ce658431b3a4541afbb0
szid2006 <sz.szabosyd@gmail.com>
2026-01-06 21:33:57 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask
 
 def create_app():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+
     app = Flask(
         __name__,
-        template_folder="templates"
+        template_folder=os.path.join(base_dir, "templates")
     )
+
     app.secret_key = "nagyon_titkos"
 
     from .auth import auth
