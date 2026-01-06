cdd3284ad79acdca0e6ddd7bdef5d544d5e27eee
szid2006 <sz.szabosyd@gmail.com>
2026-01-06 21:29:12 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask
-from .auth import auth
-from .main import main
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder="templates"
+    )
     app.secret_key = "nagyon_titkos"
 
+    from .auth import auth
+    from .main import main
     app.register_blueprint(auth)
     app.register_blueprint(main)
 
